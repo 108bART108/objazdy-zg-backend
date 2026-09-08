1db3f324614b0aa05ebcc9f9f12ddbbc7b45a957
108bART108 <bartosz.zaj@gmail.com>
2026-09-08 13:41:40 +0200

    Add files via upload

--- scrapeAll.js
+++ scrapeAll.js
@@ -42,6 +42,26 @@ function dedupeByContent(items) {
   return result;
 }
 
+// Wpis "nowy dla naszego systemu" (pierwszy raz zobaczony przez scraper)
+// to NIE to samo co "swiezo opublikowany" - zrodlo moze umiescic na
+// liscie "najnowsze" artykul sprzed miesiecy (np. gdy inne, nowsze
+// wpisy zniknely z listy, robiac miejsce starszym). W takim przypadku
+// wysylanie push "nowe utrudnienie!" wprowadzaloby w blad: uzytkownik
+// dostaje powiadomienie, ale sam wpis - sortowany scisle po dacie
+// publikacji - ladowalby na samym dole listy, wiec trudno go znalezc.
+// Push wysylamy wiec TYLKO dla wpisow, ktorych prawdziwa data
+// publikacji jest realnie niedawna (ostatnie 3 dni). Brak published_at
+// (np. scraper nie zdolal go wyciagnac) traktujemy jako "swiezy" -
+// bezpieczniejsze zalozenie niz milczec o czyms co moze byc wazne.
+const PUSH_FRESHNESS_DAYS = 3;
+function isRecentEnoughForPush(item) {
+  if (!item.published_at) return true;
+  const publishedAt = new Date(item.published_at);
+  if (isNaN(publishedAt.getTime())) return true;
+  const ageMs = Date.now() - publishedAt.getTime();
+  return ageMs <= PUSH_FRESHNESS_DAYS * 86400000;
+}
+
 async function scrapeAll() {
   const started = Date.now();
   const [fromRss, fromHtml, fromEnea, fromMzk, fromZwik, fromZdw] = await Promise.all([
@@ -82,7 +102,25 @@ async function scrapeAll() {
   // zapisany w bazie, ale pozostaje automatycznie ukryty w appce (patrz
   // listUtrudnienia w db.js) dopoki kolejne scrapowanie nie da lepszego
   // wyniku - bez zadnego recznego kroku.
-  const newItemsToNotify = newItems.filter((i) => !i.needs_review);
+  //
+  // Dodatkowo odsiewamy wpisy "nowe dla systemu", ale z data publikacji
+  // starsza niz kilka dni (patrz komentarz przy isRecentEnoughForPush) -
+  // zeby nie wysylac mylacych powiadomien o czyms, co i tak wyladuje na
+  // dole listy posortowanej po dacie.
+  const staleSkipped = [];
+  const newItemsToNotify = newItems.filter((i) => {
+    if (i.needs_review) return false;
+    if (!isRecentEnoughForPush(i)) {
+      staleSkipped.push(i);
+      return false;
+    }
+    return true;
+  });
+  if (staleSkipped.length) {
+    for (const item of staleSkipped) {
+      console.log(`[scrapeAll] pominieto push dla "nowego" ale nieswiezego wpisu [${item.category}] "${item.title}" (published_at: ${item.published_at})`);
+    }
+  }
   if (newItemsToNotify.length) {
     notifySubscribers(newItemsToNotify).catch((err) => console.error('[push] blad wysylki powiadomien:', err.message));
   }
